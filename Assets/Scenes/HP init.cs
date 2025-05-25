ba536fcb5d621cf3cdc62e1c29a894c4b34bc375
luckMillooney <165415079+luckMillooney@users.noreply.github.com>
2025-05-25 13:22:15 +0200

    upd HP init v0.1.2
    rework interface without methods

--- Assets/Scenes/HP init.cs
+++ Assets/Scenes/HP init.cs
@@ -1,83 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Threading;
 using System;
-public class HPinit : MonoBehaviour
-{
-    private int healthPoints = 3;
-    private bool damageState = false;
-    private void TestFromConsole()
-    {
-        DamageNow(Int32.Parse(Console.ReadLine()));
-        Console.WriteLine(HealthPointNow());
-    }
-    public int HealthPointNow()
-    {
-        return healthPoints;
-    }
-    public void DamageNow(int damage)
-    {
-        if (!damageState)
-        {
-            UpdHealth(damage);
-        }
-    }
-    private void UpdHealth(int damage)
-    {
-        InvincibleFrames();
-        healthPoints = healthPoints - damage;
 
-    }
-    private async void InvincibleFrames()
-    {
-        damageState = true;
-        Thread.Sleep(2000);
-        damageState = false; 
-    }
-
-// using System;
-// using System.Threading;
-
-// public class TestHP
+// public class HPinit : MonoBehaviour, IGetInfo
 // {
-//     static private int counter = Int32.Parse(Console.ReadLine());
-//     public static void Main(string[] args)
-//     {   
-//         TestFromConsole();
-//     }
-//     static private int healthPoints = 3;
-//     static private bool damageState = false;
-//     static private void TestFromConsole()
-//     {
-//         for(int i=0;i<=counter;i++){
-//         DamageNow(Int32.Parse(Console.ReadLine()));
-//         Console.WriteLine(HealthPointNow());
-        
-//         }
-//     }
-//     static public int HealthPointNow()
-//     {
-//         return healthPoints;
-//     }
-//     static public void DamageNow(int damage)
+//     [SerializeField] private int healthPoints;
+//     private int damage;
+//     [SerializeField] private bool damageState = false;
+//     public int HealthPoint => healthPoints;
+//     public bool DamageState => damageState;
+//     public int DamageSet => damage;
+
+//     public void DamageInit()
 //     {
 //         if (!damageState)
 //         {
 //             UpdHealth(damage);
 //         }
 //     }
-//     static private void UpdHealth(int damage)
+//     private void UpdHealth(int damage)
 //     {
-//         InvincibleFrames();
+//         StartCoroutine(InvicibleFrames());
 //         healthPoints = healthPoints - damage;
 
 //     }
-//     static private async void InvincibleFrames()
+
+//     IEnumerator InvicibleFrames()
 //     {
 //         damageState = true;
-//         Thread.Sleep(2000);
-//         damageState = false; 
+//         yield return new WaitForSeconds(2);
+//         damageState = false;
 //     }
+
 // }
+// public interface IGetInfo
+// {
+//     int HealthPoint { get; }
+//     bool DamageState { get; }
+//     int DamageSet { set; }
+//     }
+
+public class HPinit : MonoBehaviour
+{
+    [SerializeField] private int hp;
+    [SerializeField] private int damage;
+    [SerializeField] private bool dmgState;
+    public int HealthPoint
+    {
+        get => hp;
+        set => hp = value;
+    }
+    public bool DamageState
+    {
+        get => dmgState;
+        set => dmgState = value;
+    }
+    public int Damage
+    {
+        get => damage;
+        set => damage = value;
+    } 
+    
+    
+}
+public interface IHPInfo
+{
+    int HealthPoint { get; set; }
+    int Damage { get; set;}
+    bool DamageState { get; set; }
 }
