c03d00d9a312bdf611ef3277fd2557c7d9506ee4
luckMillooney <165415079+luckMillooney@users.noreply.github.com>
2025-06-02 22:18:10 +0200

    sonarEffectShader

---
+++ Assets/Scripts/timeShaderInit.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class timeShaderInit : MonoBehaviour
+{
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+}
